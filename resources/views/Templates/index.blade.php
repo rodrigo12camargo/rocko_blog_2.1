b2e022f92718ac7f0cdd0ed0daa11820c142b9f7
Rodrigo IS <rodrigo_1320114154@uptecamac.edu.mx>
2022-02-03 16:10:38 -0600

    proyecto

---
+++ resources/views/Templates/index.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE HTML>
+<!--
+	Read Only by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Read Only by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href=" /css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<span class="image avatar"><img src="images/avatar.jpg" alt="" /></span>
+					<h1 id="logo"><a href="#">Rodrigo Camargo</a></h1>
+					<p>El mas vejo de mi grupo <br />
+					llamado el abuelo</p>
+				</header>
+				<nav id="nav">
+					<ul>
+						<li><a href="#one" class="active">Acerca de mi</a></li>
+						<li><a href="#two">Cosas que me gusta hacer </a></li>
+						<li><a href="#three">Algunos logros de mi </a></li>
+					</ul>
+				</nav>
+				<footer>
+					<ul class="icons">
+						<li><a href="https://www.facebook.com/Rocko23635/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/camargorocko/?hl=es" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://github.com/rodrigo12camargo" class="icon brands fa-github"><span class="label">Github</span></a></li>
+					</ul>
+				</footer>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- One -->
+							<section id="one">
+								<div class="image main" data-position="center">
+									<img src="images/portada.jpg" alt="" />
+								</div>
+								<div class="container">
+									<header class="major">
+										<h2></h2>
+										<p>Una pequeña descripcion sobre lo que me gusta <br />
+										y un poco sobre mi tambien 
+									</header>
+									<p>Mi nombre es Rodrigo Jesus Camargo Gonzalez, tengo 23 años de edad y soy de la ciudad de Mexico, he vivido y crecido gran parte de mi vida en Tecamac Estado de Mexico, a los 18 años me fui a vivir y estudiar al estado de Jalisco, ahi estudie la licenciatura en cultura fisica y deportes, hasta que volvi al municipio de Tecamac y donde estudio ahora la ingenieria en software </p>
+								</div>
+							</section>
+
+						<!-- Two -->
+							<section id="two">
+								<div class="container">
+									<h3>Cosas que me gusta hacer </h3>
+									<p>Hay muchas cosas que me gustan hacer, pero estas son unas de las que mas hago o de las mas destacadas que realizo en mi dia a dia. </p>
+									<ul class="feature-icons">
+										<li class="icon solid fa-code">Escribir codigo</li>
+										<li class="icon solid fa-book">Leer libros de misterio y suspenso</li>
+										<li class="icon solid fa-coffee">Beber mucho cafe</li>
+										<li class="icon solid fa-users">Salir con amigos</li>
+									</ul>
+								</div>
+							</section>
+
+						<!-- Three -->
+							<section id="three">
+								<div class="container">
+									<h3>Algunos de mis logros</h3>
+									<p>Algunos de los logros que yo considero mas valiosos de mi area deportiva y cultural serian los siguientes </p>
+									<div class="features">
+										<article>
+											<a href="#" class="image"><img src="images/Logro1.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>Seleccion de Tecamac</h4>
+												<p>Siendo parte de la seleccion de Tecamac en balonceesto, nos logramos clasificar a participar en un torneo estatal que se jugo en Tecamac estado de Mexico donde fuimos ganadores del primer puesto. </p>
+											</div>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/Logro2.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>Seleccion de Jalisco</h4>
+												<p>Llegando a ser seleccionado en baloncesto de Jalisco, fuimos invitados a jugar en Tecamac estado de Mexico, ganando a la seleccion local y consiguiendo llevar a mas jugares becados a estudiar en jalisco.</p>
+											</div>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/Logro3.jpg" alt="" /></a>
+											<div class="inner">
+												<h4>Fotografia</h4>
+												<p>Prte de mis logros y pasatiempos favoritos es la fotografia, con la cual fui invitado a un evento de fotografos en las priamides de Teotihuacan, donde se nos ofrecio que las mejores fotografias serian publicadas </p>
+											</div>
+										</article>
+									</div>
+								</div>
+							</section>
+
+						
+					</div>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="container">
+							<ul class="copyright">
+								<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src=" /js/jquery.min.js"></script>
+			<script src=" /js/jquery.scrollex.min.js"></script>
+			<script src=" /js/jquery.scrolly.min.js"></script>
+			<script src=" /js/browser.min.js"></script>
+			<script src=" /js/breakpoints.min.js"></script>
+			<script src=" /js/util.js"></script>
+			<script src=" /js/main.js"></script>
+
+	</body>
+</html>
